ac9c57e8a20d3a16d9e0e5016aff945182864a2f
pooja <https://github.com/poojatayde9960/ecom.git>
2025-01-20 13:30:16 +0530

    let_const

---
+++ 2_Let_const.js
@@ -0,0 +1,34 @@
+//imp
+
+// redeclaratoin
+// scop
+// var x
+
+// var x
+
+// let y
+// let y
+
+// const x
+// const x = "dell"
+
+//let block scope
+// var functional/global scope
+// var x = 10
+// let y = 10
+
+// if (true) {
+//     const price = 500
+//     var x = 100
+//     let y = 100
+//     console.log(y)
+// }
+// console.log(x)
+// console.log(y)
+// console.log(price)
+
+console.log(x) //error bcoz of block scope
+console.log(y)
+
+var x = 10//error bcoz of block scope
+let y = 10//error bcoz of block scope
