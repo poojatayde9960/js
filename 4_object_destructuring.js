7039fb4b252be613a1e9cd95a938dbaf55fa3c7f
pooja <https://github.com/poojatayde9960/ecom.git>
2025-01-20 16:07:25 +0530

    object_destructuring

---
+++ 4_object_destructuring.js
@@ -0,0 +1,9 @@
+const obj = { name: "john", age: 20, city: "london" }
+const { name: fname, age, city, test } = obj
+
+
+
+console.log(fname);
+console.log(age);
+console.log(city);
+console.log(test);
